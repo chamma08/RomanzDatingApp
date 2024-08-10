c92cecaa6c71e2fe09d24ecadef44c15897e36df
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2024-08-10 08:23:36 +0530

    updated

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import { Redirect } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
+      <Redirect to="/onboarding" />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
